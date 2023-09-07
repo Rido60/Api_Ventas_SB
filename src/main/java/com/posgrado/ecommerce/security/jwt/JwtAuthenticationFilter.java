6531cf0ae36c739b6a7355882b525d0ac9bd02f5
rido60 <jrmjrido@gmail.com>
2023-09-07 17:09:45 -0400

    authentication corrected

--- src/main/java/com/posgrado/ecommerce/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/posgrado/ecommerce/security/jwt/JwtAuthenticationFilter.java
@@ -25,7 +25,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
       FilterChain filterChain
   ) throws ServletException, IOException {
     String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
-    //Bearer guyftdcjvhjvfytdytcgvgh9-hvjhvhk-hhvjvj
+    //Bearer
     if (authHeader == null || !authHeader.startsWith("Bearer ") ){
       filterChain.doFilter(request, response);
       return;
